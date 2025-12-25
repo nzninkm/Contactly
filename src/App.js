712de9953333d17d4a46d006d2cf5f0139530f2b
Nazaniinkarimian@gmail.com <nazaniinkarimian@gmail.com>
2025-12-25 16:13:43 +0330

    integrate Bootstrap and Font Awesome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="App">
-      <h3>اپلیکیشن مدیریت مخاطبین</h3>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <h3>اپلیکیشن مدیریت مخاطبین</h3>
+            <span className="fa fa-user"></span>
+            <span className="fas fa-address-book"></span>
+            <button className="btn btn-primary">Button</button>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+//Bootstrap
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,4 +13,4 @@ root.render(
 );
 
 
-reportWebVitals();
+
