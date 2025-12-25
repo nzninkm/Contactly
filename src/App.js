131eb954ee264faf5687d8368c61668619a35fa5
Nazaniinkarimian@gmail.com <nazaniinkarimian@gmail.com>
2025-12-25 18:21:41 +0330

    Create Navbar and Search components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+import Navbar from "./components/Navbar";
 import './App.css';
 
 const App = () => {
     return (
         <div className="App">
-
-            <h3>اپلیکیشن مدیریت مخاطبین</h3>
-            <span className="fa fa-user"></span>
-            <span className="fas fa-address-book"></span>
-            <button className="btn btn-primary">Button</button>
+            <Navbar/>
         </div>
     );
 }
---
+++ src/components/contact/SearchContact.jsx
@@ -0,0 +1,12 @@
+
+const SearchContact = () => {
+    return (
+        <div className="input-group mx-2 w-75" dir="ltr">
+            <span className="input-group-text" id="basic-addon1" style={{backgroundColor: "purple"}}>
+                <i className="fas fa-search"/>
+            </span>
+        <input dir="rtl" type="text" style={{backgroundColor: "gray", bordercolor: "purple"}} className="form-control" placeholder="جستجوی مخاطب"/>
+    </div>
+    )
+}
+export default SearchContact;
